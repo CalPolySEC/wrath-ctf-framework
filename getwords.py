b25ce497eb6ce6ce3480ae5dfb64c5bbdb25770f
Max Zinkus <maxzinkus@gmail.com>
2016-01-31 00:59:59 -0800

    TempleScript

--- getwords.py
+++ getwords.py
@@ -1,7 +1,11 @@
 from subprocess import getoutput
+from random import randrange
 
 DICT_PATH = './dict.txt'
 
+OOPS_SEEK_TOO_FAR = 48
+
+# don't run on OS X
 def randomize():
     out = getoutput('sort -R ' + DICT_PATH)
     with open(DICT_PATH, 'w') as f:
@@ -9,9 +13,12 @@ def randomize():
     f.close()
 
 def getwords():
-    randomize()
-    out = getoutput('head -n 3 ' + DICT_PATH)
-    return '_'.join(out.split('\n')[:3])
+    with open(DICT_PATH, 'r') as f:
+        f.seek(randrange(0, int(getoutput('wc -l ' + DICT_PATH).split(' ')[0])-OOPS_SEEK_TOO_FAR))
+        out = f.readlines(OOPS_SEEK_TOO_FAR)
+        out = [x.replace('\n', '') for x in out]
+    print(out)
+    return '_'.join(out[1:4])
 
 if __name__ == '__main__':
     print(getwords())
