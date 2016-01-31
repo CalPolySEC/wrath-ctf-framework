144d705caac1c7d47e6942e9b252d016a75beead
Max Zinkus <maxzinkus@gmail.com>
2016-01-31 01:03:10 -0800

    Magic numbers

--- getwords.py
+++ getwords.py
@@ -5,6 +5,8 @@ DICT_PATH = './dict.txt'
 
 OOPS_SEEK_TOO_FAR = 48
 
+DICT_LENGTH = 61973
+
 # don't run on OS X
 def randomize():
     out = getoutput('sort -R ' + DICT_PATH)
@@ -14,10 +16,9 @@ def randomize():
 
 def getwords():
     with open(DICT_PATH, 'r') as f:
-        f.seek(randrange(0, int(getoutput('wc -l ' + DICT_PATH).split(' ')[0])-OOPS_SEEK_TOO_FAR))
+        f.seek(randrange(0, int(getoutput(DICT_LENGTH-OOPS_SEEK_TOO_FAR))
         out = f.readlines(OOPS_SEEK_TOO_FAR)
         out = [x.replace('\n', '') for x in out]
-    print(out)
     return '_'.join(out[1:4])
 
 if __name__ == '__main__':
