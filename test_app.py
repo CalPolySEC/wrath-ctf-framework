583a05adfa8e472cfe5c71c0367ced71d96b72b5
Nick Frost <nickfrostatx@gmail.com>
2016-02-02 16:24:23 -0800

    Test for trailing slashes

--- test_app.py
+++ test_app.py
@@ -32,13 +32,14 @@ def test_error(client):
 
 
 def test_static_pages(client):
-    for url in ('/login', '/about', '/contact'):
+    for url in ('/login/', '/about/', '/contact/'):
         rv = client.get(url)
         assert rv.status_code == 200
 
 
 def test_team_404(client):
-    for url in ('/teams/1', '/teams/0', '/teams/-1', '/teams/asdf'):
+    for url in ('/teams/1/', '/teams/0/', '/teams/-1/', '/teams/a',
+                '/teams/a/'):
         rv = client.get(url)
         assert rv.status_code == 404
 
@@ -48,17 +49,17 @@ def test_new_team(client):
         'name': 'Sgt. Pepper\'s Lonely Hearts Club Band'
     })
     assert rv.status_code == 303
-    assert rv.headers['Location'] == 'http://localhost/teams/1'
+    assert rv.headers['Location'] == 'http://localhost/teams/1/'
 
-    rv = client.get('/teams/1')
+    rv = client.get('/teams/1/')
     assert b'Team successfully created.' in rv.data
-    assert b'<h1>Sgt. Pepper&#39;s Lonely Hearts Club Band</h1>' in rv.data
+    assert b'>Sgt. Pepper&#39;s Lonely Hearts Club Band<' in rv.data
 
 
 def test_new_team_missing_name(client):
     rv = client.post('/teams')
     assert rv.status_code == 303
-    assert rv.headers['Location'] == 'http://localhost/login'
+    assert rv.headers['Location'] == 'http://localhost/login/'
     assert b'You must supply a team name.' in client.get('/').data
 
 
@@ -66,7 +67,7 @@ def test_new_team_duplicate(client):
     rv = client.post('/teams', data={'name': 'Some Team Name'})
     rv = client.post('/teams', data={'name': 'sOmE tEaM nAme'})
     assert rv.status_code == 303
-    assert rv.headers['Location'] == 'http://localhost/login'
+    assert rv.headers['Location'] == 'http://localhost/login/'
     assert b'That team name is taken.' in client.get('/').data
 
 
@@ -74,7 +75,7 @@ def test_login(client):
     def assert_bad_login(data, message):
         rv = client.post('/auth_team', data=data)
         assert rv.status_code == 303
-        assert rv.headers['Location'] == 'http://localhost/login'
+        assert rv.headers['Location'] == 'http://localhost/login/'
         assert message in client.get('/').data
 
     assert_bad_login({}, b'No team exists with that name.')
@@ -88,13 +89,13 @@ def test_login(client):
 
     password = Team.query.filter_by(id=1).first().password
     rv = client.post('/auth_team', data={'name': 'ABC', 'password': password})
-    assert rv.headers['Location'] == 'http://localhost/teams/1'
+    assert rv.headers['Location'] == 'http://localhost/teams/1/'
 
 
 def test_logout_unauthed(client):
     rv = client.get('/logout')
     assert rv.status_code == 303
-    assert rv.headers['Location'] == 'http://localhost/login'
+    assert rv.headers['Location'] == 'http://localhost/login/'
 
 
 def test_logout_bad_token(client):
@@ -118,13 +119,13 @@ def test_logout(client):
 
 
 def test_submit_page(client):
-    rv = client.get('/submit')
+    rv = client.get('/submit/')
     assert rv.status_code == 303
-    assert rv.headers['Location'] == 'http://localhost/login'
+    assert rv.headers['Location'] == 'http://localhost/login/'
 
     auth(client)
 
-    rv = client.get('/submit')
+    rv = client.get('/submit/')
     assert rv.status_code == 200
 
 
@@ -132,7 +133,7 @@ def test_flag_submission(client):
     def assert_flag(flag, msg):
         rv = client.post('/flags', data={'flag': flag})
         assert rv.status_code == 303
-        assert rv.headers['Location'] == 'http://localhost/submit'
+        assert rv.headers['Location'] == 'http://localhost/submit/'
         assert msg in client.get('/').data
 
     # SHA-256 of "fleg"
