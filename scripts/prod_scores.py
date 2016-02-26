260aac3466e6691b938ef4551861c3402d16d21b
Nick Frost <nickfrostatx@gmail.com>
2016-02-26 15:58:47 -0800

    Fix prod_scores.py

--- scripts/prod_scores.py
+++ scripts/prod_scores.py
@@ -15,7 +15,7 @@ def load_passwords(in_file):
     with subprocess.Popen(['/usr/bin/openssl', 'enc', '-d', '-aes-256-cbc'],
         stdin=in_file, stdout=subprocess.PIPE) as proc:
         for p in proc.stdout:
-            yield hashlib.sha256(p).hexdigest()
+            yield hashlib.sha256(p.strip()).hexdigest()
 
 
 def main(argv):
@@ -24,16 +24,16 @@ def main(argv):
         return 1
     with open(argv[2], 'r') as score_file:
         scores = json.load(score_file)
+        wargame = scores['wargame']
+        points = sorted([(int(k), v) for k, v in scores['PointsByLevel'].items()])
     with open(argv[3], 'rb') as pw_file:
         passwords = load_passwords(pw_file)
         db.create_all()
-        cat = Category(name=scores['wargame'], order=int(argv[1]))
+        cat = Category(name=wargame, order=int(argv[1]))
         db.session.add(cat)
-        #for level, points in sorted((int(l), p) for l, p in scores['PointsByLevel'].items()):
-        for flag_hash in passwords:
-            level = int(level)
-            print(level, points)
-            flag = Flag(hash=flag_hash, level=level, category=cat, points=points)
+        for level, flag_hash in enumerate(passwords):
+            print(level, points[level], flag_hash)
+            flag = Flag(hash=flag_hash, level=level, category=cat, points=points[level][1])
             db.session.add(flag)
         db.session.commit()
         return 0
