5d7ae892c034e7f75b5d9a2af2f77b851f3bb98c
Max Zinkus <mzinkus@calpoly.edu>
2016-03-03 13:59:20 -0800

    Edit scripts

--- scripts/build_db.py
+++ scripts/build_db.py
@@ -3,14 +3,16 @@
 import hashlib
 # This here is a hack
 import sys
-sys.path.extend('..')
+sys.path.extend('..') # jesus
 from app import app, db, Category, Flag, Team
+
+TEST_CATEGORIES = [('Bandit', 26), ('Leviathan', 7), ('Lock Picking', 0), ('Teardown', 0)]
 
 def main():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     db.create_all()
-    for cat, levels, order in (('Bandit', 26, 0), ('Leviathan', 7, 1), ('Lock Picking', 0, 2), ('Teardown', 0, 3)):
-        c = Category(name=cat, order=order)
+    for index, (cat, levels) in enumerate(TEST_CATEGORIES):
+        c = Category(name=cat, order=index)
         db.session.add(c)
         for level in range(levels + 1):
             flag = Flag(hash=hashlib.sha256(b'%s %d' % (cat.encode(), level)).hexdigest(), level=level, category=c, points=10)
