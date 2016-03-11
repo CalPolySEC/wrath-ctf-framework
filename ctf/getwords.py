57a7cc4f56ec6ccf99c09d3dc6fc4113b37ffdd6
Max Zinkus <mzinkus@calpoly.edu>
2016-03-11 01:42:15 -0800

    PasswordFactory++

--- ctf/getwords.py
+++ ctf/getwords.py
@@ -6,9 +6,13 @@ import os
 
 LOCK_PATH = '/tmp/wordlist_dict.lock'
 DICT_PATH = './dict.txt'
+PW_LENGTH = 3
 
 
 def randomize():
+    """ black magic to randomize a dictionary file
+        using command-line sort(8)
+        Don't worry, we lock the dict file """
     if getoutput('uname') != "Linux" and 'n' in input("Your OS might not support `sort -R`, proceed? [Yn] ").lower():
         print("Exiting")
         return
@@ -19,22 +23,34 @@ def randomize():
         f.close()
 
 class PasswordFactory:
+    """ Password FACTORY. 
+        IT'S A FACTORY. """
+
     def refresh(self):
+        """ delicious randomized steak
+            with a side of effects """
         randomize()
         self.passwords = getwords()
         self.remaining = int(len(passwords) / 3)
 
     def get_pass(self):
+        """ Client-facing code, so it
+            has to be pretty. Naturally,
+            build strings, pop from arrays,
+            and generally enterprise around
+            while state flies left and right """
         if self.remaining == 0:
             self.refresh()
         pw = ""
-        for i in range(2):
+        for i in range(PW_LENGTH - 1):
             pw += self.passwords.pop()
             pw += '_'
         pw += self.passwords.pop()
         self.remaining -= 1
 
 def getwords():
+    """ Bullshit: pull the ENTIRE FILE
+        into memory """
     with open(DICT_PATH, 'r') as f:
         f.readlines()
 
