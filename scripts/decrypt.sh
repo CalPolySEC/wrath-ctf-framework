1c17debef9f5d43bfd3cf26276efa77eb741b683
Max Zinkus <mzinkus@calpoly.edu>
2016-03-11 03:08:02 -0800

    Python API to encrypt & decrypt

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# usage: ./decrypt.sh <infile> <outfile>
-openssl enc -d -in $1.enc -out $2.txt -aes-256-cbc
+# usage: ./decrypt.sh <infile> <outfile> <pass>
+openssl enc -d -in $1.enc -out $2.txt -pass $3 -aes-256-cbc
---
+++ scripts/fcrypt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import subprocess
+import hashlib
+import FileLock
+
+ENCRYPT="./encrypt.sh"
+DECRYPT="./decrypt.sh"
+LOCK_PATH="/tmp/pysha_crypt.lock"
+
+
+def fcrypt(CRYPT, fin, fout, pw):
+    subprocess.call([CRYPT, fin, fout, pw])
+
+def encrypt(fin, fout, pw):
+    fcrypt(ENCRYPT, fin, fout, pw)
+
+def decrypt(fin, fout, pw):
+    fcrypt(DECRYPT, fin, fout, pw)
