e08e7285765160a3955241bc632ddd5a1dcdef2e
nickago <ngonella@calpoly.edu>
2016-09-13 12:43:44 -0700

    Removed debug statement

--- ctf/setup.py
+++ ctf/setup.py
@@ -16,7 +16,6 @@ def build_challenges():
             try:
                 config = json.load(config_file)
             except ValueError:
-                print "here"
                 raise ValueError("%s was malformed" % config_file)
             for problem in config["problems"]:
                 # We put this first to avoid circular dependancies
