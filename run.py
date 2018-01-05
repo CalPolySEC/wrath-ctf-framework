349f909144d6697ca5c4c609b653e7d9eedde75b
atti1a <ngonella@calpoly.edu>
2018-01-05 15:32:23 -0800

    Add libsodium to travis env

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from ctf import create_app
 import os
 app = create_app()
-app.run(debug=True, port=int(os.environ.get('PORT', '5000')))
+app.run(debug=True,
+        host=os.environ.get('HOST', '127.0.0.1'),
+        port=int(os.environ.get('PORT', '5000')))
